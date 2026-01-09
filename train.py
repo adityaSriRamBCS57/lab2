418dbca16824aa54b86de9599f98d251e552323d
adityaSriRamBCS57 <adityasrirambarnala@gmail.com>
2026-01-09 21:52:53 +0530

    Ridge, alpha : 1.0, No Scaling, test_size  : 0.4, random_state : 42

--- train.py
+++ train.py
@@ -16,13 +16,12 @@ df = pd.read_csv("data/winequality-red.csv", sep=";")
 X = df.drop("quality", axis=1)
 y = df["quality"]
 
-# Preprocessing (SCALER)
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+
+X_scaled = X.values
 
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled, y, test_size=0.2, random_state=42
+    X_scaled, y, test_size=0.4, random_state=42
 )
 
 # Model (RIDGE)
